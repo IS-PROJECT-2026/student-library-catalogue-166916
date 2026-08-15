35289dc71fb5f4fbcb75443d1b4fcfa608c22c48
zandykanani <zandykanani@gmail.com>
2026-08-15 18:45:02 +0300

    chore: set up project structure

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const bookList = document.getElementById("book-list");
+
+    bookList.innerHTML = `
+        <p>The library catalogue is ready for development.</p>
+    `;
+});
